2e824e3c92ce30e29ec01599a0e1bd8c8a80f2b8
Omri Shapira <omri.shapira@gmail.com>
2025-08-02 18:17:42 +0300

    Initial Commit

---
+++ inventory-listTests/inventory_listTests.swift
@@ -0,0 +1,17 @@
+//
+//  inventory_listTests.swift
+//  inventory-listTests
+//
+//  Created by Omri Shapira on 02/08/2025.
+//
+
+import Testing
+@testable import inventory_list
+
+struct inventory_listTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
